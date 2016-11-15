ab3e32786be233719a09ba5a6d577b9955a2f2c1
胡贝贝 <hubei@hubeibeidemacbook-pro.local>
2016-11-15 14:57:14 +0800

    Initial Commit

---
+++ MusicPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MusicPlayer
+//
+//  Created by 胡贝贝 on 16/11/15.
+//  Copyright © 2016年 胡贝贝. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MusicPlayer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MusicPlayer
+//
+//  Created by 胡贝贝 on 16/11/15.
+//  Copyright © 2016年 胡贝贝. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
